9df674bdcdfc4dc329883e3cad4d3e08c22daf8d
ThomasVandenhede <thomasvandenhede@hotmail.com>
2019-07-29 22:00:26 +0200

    Import boostrap through npm instead of using cdn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import HomePage from "./pages/home";
 import DashboardPage from "./pages/dashboard";
