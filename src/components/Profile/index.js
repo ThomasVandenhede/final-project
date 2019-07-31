05d1f574618b6124b6f022b846e43a94d0f4b536
ThomasVandenhede <thomasvandenhede@hotmail.com>
2019-07-31 08:23:11 +0200

    Update profile card

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 import { Auth } from "../../context";
 
@@ -8,23 +8,27 @@ const Profile = ({ user }) => (
   <Auth.Consumer>
     {({ currentUser }) => (
       <Card>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Img
+          variant="top"
+          src={user.picture}
+          style={{ maxWidth: "150px", margin: "1em auto 0", padding: "1em" }}
+        />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{user.username}</Card.Title>
           <Card.Text>
-            {user.description || (
-              <>
-                Tu n'as pas écrit de description...
-                <br />
-                C'est peut-être le moment ? <br />
-                <Link to="/settings">Ajouter une description</Link>
-              </>
-            )}
+            {currentUser.id === user.id
+              ? user.description || (
+                  <>
+                    Tu n'as pas écrit de description...
+                    <br />
+                    C'est peut-être le moment ? <br />
+                    <Link to="/settings">Ajouter une description</Link>
+                  </>
+                )
+              : user.description ||
+                `${user.username} n'a pas encore de description`}
           </Card.Text>
         </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-        </ListGroup>
         <Card.Body>
           {!!(user.friends && user.friends.length) ? (
             <Link to="/friends">Voir mes amis</Link>
